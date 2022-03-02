4ec0a2a8d130810cdafdbb2a2375bd4950e2a5fc
MirKiparis <90731391+MirKiparis@users.noreply.github.com>
2022-03-03 01:16:26 +0300

    Update num4.py

--- лаборатория1/num4.py
+++ лаборатория1/num4.py
@@ -5,7 +5,7 @@
 Удалить файл.
 """
 import xml.etree.cElementTree as ET
-
+import os
 root = ET.Element("root")
 doc = ET.SubElement(root, "doc")
 
@@ -15,3 +15,16 @@ ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
 tree = ET.ElementTree(root)
 tree.write("filename.xml")
 
+tree = ET.parse('filename.xml')
+root = tree.getroot()
+doc = root[0]
+for child_of_root in doc:
+    # print(child_of_root.tag, child_of_root.attrib)
+    print(child_of_root.tag, '>> измените текст >> ')
+    child_of_root.text = input()
+
+tree.write("filename.xml")
+f = open('filename.xml', 'r')
+print(f.read())
+f.close()
+os.remove("filename.xml")
